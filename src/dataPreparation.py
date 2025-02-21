64016e1c2c7a98fdd7b7693aa2a743435f960448
ABlezinger <blezingeralexander@yahoo.de>
2025-02-21 15:06:46 +0100

    binary chest

--- src/dataPreparation.py
+++ src/dataPreparation.py
@@ -60,10 +60,20 @@ def get_PneumoniaMNIST() -> tuple[DataLoader, DataLoader, DataLoader, int]:
 def get_ChestMNIST() -> tuple[DataLoader, DataLoader, DataLoader, int]:
     if not os.path.exists(data_path):
         os.makedirs(data_path)
+    
+    def multi_to_binary_chest(dataset: Dataset):
+        labels = dataset.labels
+        new_lables = ar = np.array([np.where(np.sum(row) > 0, 1, 0) for row in dataset.labels]).reshape(-1, 1)
+        dataset.labels = new_lables
+        return dataset
 
     train = ChestMNIST(split="train", download=True, root=project_path + "/data", transform=MNIST_preprocess)
+    train = multi_to_binary_chest(train)
     val = ChestMNIST(split="val", download=True, root=project_path + "/data", transform=MNIST_preprocess)
+    val = multi_to_binary_chest(val)
     test = ChestMNIST(split="test", download=True, root=project_path + "/data", transform=MNIST_preprocess)
+    test = multi_to_binary_chest(test)
+
 
 
     train_loader = DataLoader(train, batch_size=16, shuffle=True)
@@ -71,6 +81,7 @@ def get_ChestMNIST() -> tuple[DataLoader, DataLoader, DataLoader, int]:
     test_loader = DataLoader(test, batch_size=16, shuffle=True)
 
     num_classes = len(np.unique(train.labels))
+
     return train_loader, val_loader, test_loader, num_classes
   
 def get_TissueMNIST() -> tuple[DataLoader, DataLoader, DataLoader, int]:
@@ -87,3 +98,10 @@ def get_TissueMNIST() -> tuple[DataLoader, DataLoader, DataLoader, int]:
 
     num_classes = len(np.unique(train.labels))
     return train_loader, val_loader, test_loader, num_classes
+
+
+if __name__ == "__main__":
+    t, v, test, n = get_ChestMNIST()
+
+    print(t.dataset.labels)
+    print(n)
